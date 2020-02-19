1efc26994e64f8044653ab97d2c252af80d9de89
Noriuki <lanezupiroli@gmail.com>
2020-02-19 00:02:24 -0300

    Primeiros Ajustes

---
+++ src/components/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="d-flex-wrap" id="home">
+    <v-row>
+        <v-text-field label="input"></v-text-field>
+        <v-btn large color="primary">Primary</v-btn>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"home",
+}
+</script>
+
+<style>
+    #home{
+        display: flex;
+        flex-grow: 1;
+    }
+    #home>.row{
+        max-width: 60%;
+        margin: auto;
+    }
+</style>
